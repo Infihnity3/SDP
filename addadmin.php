855f670fba64a590785d0596b071f5a64ba28307
ZackChow <zackchow63@gmail.com>
2020-03-20 23:41:11 +0800

    added add admin function(partial)

--- addadmin.php
+++ addadmin.php
@@ -22,14 +22,39 @@
 
 <body style="background-color: rgb(241,247,252);">
   <?php include "nav2.php" ?>
+  <?php require "conn.php";
+  if(isset($_POST['user']) && isset($_POST['password'])){
+    $user = $_POST['adminname'];
+    $id = $_POST['adminic'];
+    $gender = $_POST['admingender'];
+    $dob = $_POST['admindob'];
+    $phone = $_POST['adminphone'];
+    $address = $_POST['adminaddress'];
+    $email = $_POST['adminemail'];
+    $password = $_POST['adminpassword1'];
+
+
+    $query = "INSERT INTO 'users' (Name ,IC ,Gender, DoB, PhoneNum, Email, password, Address) 
+        VALUES('$user', ' $id ', '$gender','$dob','$phone','$address','$email','$password')";
+    $result = mysqli_query($query,$conn);
+    if($result){
+        $msg = "Registered Sussecfully";
+    }
+    else
+        $msg = "Error Registering";
+}
+?>
     <div style="margin-left: 350px;margin-top: 30px; margin-bottom: 30px;width: 800px;">
         <div class="container" style="width: 800px;margin: 0px;margin-right: 0px;margin-left: 0px;background-color: #ace7ef;background-image: url(&quot;assets/img/background.jpg&quot;);background-repeat: no-repeat;background-size: cover;">
             <div class="row add" style="width: 800px;background-color: rgba(0,0,0,0.37);">
                 <div class="col-md-12">
                     <form style="background-repeat: no-repeat;background-size: auto;">
                         <h3 class="text-center" style="color: rgb(237,237,237);">Add Admin</h3>
-                        <div class="form-group text-left"><input class="form-control" type="text" style="width: 300px;margin-left: 230px;background-color: rgb(247,249,252);" name="adminname" placeholder="Fullname"></div>
-                        <div class="form-group text-left"><input class="form-control" type="text" style="width: 300px;margin-left: 230px;background-color: rgb(247,249,252);" name="adminic" placeholder="IC Number"></div>
+                        <div class="form-group text-left">
+                            <input class="form-control" type="text" style="width: 300px;margin-left: 230px;background-color: rgb(247,249,252);" name="adminname" placeholder="Fullname">
+                        </div>
+                        <div class="form-group text-left">
+                            <input class="form-control" type="text" style="width: 300px;margin-left: 230px;background-color: rgb(247,249,252);" name="adminic" placeholder="IC Number"></div>
                         <div class="form-group"><select class="form-control" required="" style="width: 300px;margin-left: 230px;" name="admingender"><option value="None">Choose your gender</option><option value="Male">Male</option><option value="Female">Female</option></select></div>
                         <div
                             class="form-group">
